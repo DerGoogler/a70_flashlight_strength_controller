728aea2aca6a2f0d08ca29df8a3f5fb2be57af20
Der_Googler <support@dergoogler.com>
2022-06-20 22:50:37 +0200

    Update

--- app/src/main/cpp/Android.mk
+++
@@ -1,7 +0,0 @@
-LOCAL_PATH := $(call my-dir)
-include $(CLEAR_VARS)
-LOCAL_MODULE_TAGS := optional
-LOCAL_PACKAGE_NAME := treble-flash-samsung-a70
-LOCAL_MODULE_PATH := $(TARGET_OUT_PRODUCT)/overlay
-LOCAL_PRIVATE_PLATFORM_APIS := true
-include $(BUILD_PACKAGE)
---
+++ app/src/main/java/com/dergoogler/phh/flashlight/Receivers/FlashlightReceiver.java
@@ -0,0 +1,41 @@
+package com.dergoogler.phh.flashlight.Receivers;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.dergoogler.phh.flashlight.controllers.FlashlightController;
+
+public class FlashlightReceiver extends BroadcastReceiver {
+    private FlashlightController fc;
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Bundle extras = intent.getExtras();
+        fc = new FlashlightController(context);
+        if (hasFlashlight()) {
+            boolean state = extras.getBoolean("enable");
+            if (isAvailable()) {
+                setFlashlight(state);
+            }
+        }
+    }
+
+    private boolean isAvailable() {
+        return fc.isAvailable();
+    }
+
+    private boolean hasFlashlight() {
+        return fc.hasFlashlight();
+    }
+
+    private boolean isEnabled() {
+        return fc.isEnabled();
+    }
+
+    private void setFlashlight(boolean enabled) {
+        fc.setFlashlight(enabled);
+    }
+
+}
